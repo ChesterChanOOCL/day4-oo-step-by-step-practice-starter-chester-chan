c9895cd15c2b1d22e04abe1c319f24e94cc7e087
CHANCH10 <chester.chan@oocl.com>
2024-11-21 21:55:11 +0800

    feat: added new school class

--- src/main/java/oo/School.java
+++ src/main/java/oo/School.java
@@ -1,4 +1,19 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class School {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+
+    public School() {
+        this.teachers = new ArrayList<>();
+        this.students = new ArrayList<>();
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
 }
